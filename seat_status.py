9703c8a79217627bad4a31b71ab331c4cb0da61a
SethiAbhinav <abhinavabhinav33@gmail.com>
2021-11-20 20:36:44 +0530

    BEST CODE

---
+++ seat_status.py
@@ -0,0 +1,118 @@
+import cv2
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+import os
+from object_detection import Object_detect
+# SAVING SYNTAX
+# rois : {"cam_num" : [
+#                          [bottom_right_x, bottom_right_y, top_left_x, top_left_y]
+#                    ]}
+rois = {
+        '2' : [ [210,255,145,160], [290,255,210,160], [270,170,210,110], [210,180,140, 85] ],
+
+        '6' : [ [145,250,85,155], [160,300,80,230] ],
+
+        '9' : [ [75,240,8,155], [150,280,68,170], [130,175,80,120], [70,168,30,127], [240,270,145,160], [300,230,230,140], [290,165,215,115]], # table-1: 1,2,3,4 ; table 2: 5,6,8
+
+        '10' : [[170,255,120,165], [210,225,160,150], [160, 160, 132,110], [140,180,90,120] ],
+
+        '25' : [ [205,180,160,80], [300,155,265,90], [335,155,295,90] ],    
+                }
+
+no_person_rois = { '2' : [ [210,215,150,160], [280,210,215,160], [270,165,220,120], [210,180,140,140] ],
+                   '6' : [ [145,250,85,190], [160,265,80,220] ],
+                   '9' : [ [77,220,25,160], [100,215,65,170], [105,170,75,140], [80,165,51,142], [240, 210, 200,165] , [265,205,225,155], [248,165,214,130], ], # table-2: 7 was unobservable.
+                   '10' : [ [165,195,120,150], [190,180,140,140], [170, 170, 132,130], [150,175,95,140]], 
+                  '25' : [ [195,110,160,80], [300,115,265,90], [335,120,295,90] ]
+                  }  
+seat_status_indicator = {'empty' : 0, 'occupied' : 1, 'on hold' : 2}
+
+def check_table_roi(cam_num, idx, img):
+    table_section = no_person_rois[cam_num][idx]
+    df = Object_detect(img[table_section[3]:table_section[1],table_section[2]:table_section[0],:], confThreshold=0.3, nmsThreshold=0.5)
+    if df.empty:
+        status = 'empty'
+    else:
+        status = 'on hold'
+    return status
+
+
+def load_images_from_folder(folder):
+    final_df = pd.DataFrame(columns = ['Camera Number', 'Chair Number', 'Status' ])
+    for filename in os.listdir(folder):
+        cam_num = filename.split('Camera')[1]
+        cam_num = cam_num.split('_')[0]
+        if cam_num in ['2','6','9','10','25']: #ground floor cams
+            img = cv2.imread(os.path.join(folder, filename))
+            cv2.imshow('img',img)
+            cv2.waitKey(0)
+            # resize image - rois are defined on this size
+            img = cv2.resize(img , (352, 288)) 
+
+            # check if image is empty
+            if img is not None:
+                roi = rois[cam_num]
+
+                for idx, chair in enumerate(roi):
+
+                    # Initialize flag and status to default values for each chair
+                    flag = 0
+                    status = 'empty'
+                    print(f"{filename} CAM_NUM: {cam_num} CHAIR : {idx + 1}")
+                    # print(chair[3],chair[1],chair[2],chair[0])
+                    
+                    # calling Object_detect on ROI
+                    cv2.imshow('img',img[chair[3]:chair[1],chair[2]:chair[0]])
+                    cv2.waitKey(2)
+                    
+                    df = Object_detect(img[chair[3]:chair[1],chair[2]:chair[0],:], confThreshold=0.3, nmsThreshold=0.5)
+                    # print('in seat status:' ,df)
+
+                    # check if df is empty
+                    if df.empty:
+                        # print("\n\nRECHECKING\n")
+                        status = check_table_roi(cam_num, idx, img)
+                    else:
+                        if 1 in df['ClassIds'].values:
+                            status = 'occupied'
+                        else:
+                            unique_vals = df['ClassIds'].unique()
+                            for item in unique_vals:
+                                if item not in [57,69,70,71,73,14]: # ignore chairs
+                                    status = 'on hold'
+                                    flag = 1
+                                    break
+                            
+                            if flag == 0:
+                                # print("rechecking cuz we got only chair")
+                                status = check_table_roi(cam_num, idx, img)
+                                
+                    # print(f"STATUS : {status} {seat_status_indicator[status]}")
+                    final_df = final_df.append({'Camera Number' : cam_num, 'Chair Number' : idx + 1, 'Status' : seat_status_indicator[status]}, ignore_index=True)
+                    if idx == 1:
+                        print("OPPPPPPPPPPPP")
+                        break
+                
+    return final_df
+
+# change folder name to: 'f1', 'f2', 'f3' to test other images
+folder_name = 'f3'
+final_df = load_images_from_folder(folder_name)
+print("I am done")
+
+# changing type to 'int' so as to sort by camera number
+final_df = final_df.astype(int)
+final_df = final_df.sort_values(by=['Camera Number', 'Chair Number'], ascending=True)
+
+# save final_df in a csv file
+final_df.to_csv('seat_status.csv', index = False)
+
+'''
+                        RESULTS:
+Folder f1: 15/20
+Folder f2: 15/20
+Folder f3: 16/20
+Folder f4: 15/20
+'''
