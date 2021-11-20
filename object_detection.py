99cc1c8ddaca168c6781fbc9a5ba60bcd687543b
TheBlackCat22 <sushil.vemuri22@gmail.com>
2021-11-20 23:25:12 +0530

    yahalloo

--- object_detection.py
+++ object_detection.py
@@ -5,29 +5,33 @@ import pandas as pd
 #Object detection function 
 def Object_detect(frame,confThreshold=0.4,nmsThreshold=0.3):
     #Download weights yolov4-p6.weights from https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-p6.weights
+    #pretrained object detection model called yolov4-p6
     net=cv2.dnn.readNet("yolov4-p6.cfg","yolov4-p6.weights")
     outNames = net.getUnconnectedOutLayersNames()
+    
+    #Unpacking the output of the neural network
     def postprocess(frame, outs):
         frameHeight = frame.shape[0]
         frameWidth = frame.shape[1]
 
-        
-        # def drawPred(classId, conf, left, top, right, bottom):
-        #     cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0))
+        #If we want to draw bounding boxes around detected objects
+        '''
+        def drawPred(classId, conf, left, top, right, bottom):
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0))
 
-        #     label = '%.2f' % conf
+            label = '%.2f' % conf
 
-        #     if classes:
-        #         assert(classId < len(classes))
-        #         label = '%s: %s' % (classes[classId-1], label)
+            if classes:
+                assert(classId < len(classes))
+                label = '%s: %s' % (classes[classId-1], label)
 
-        #     labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-        #     top = max(top, labelSize[1])
-        #     cv2.rectangle(frame, (left, top - labelSize[1]), (left + labelSize[0], top + baseLine), (255, 255, 255), cv2.FILLED)
-        #     cv2.putText(frame, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-        #     cv2.imshow("frame",frame)
-        #     cv2.waitKey(0)
-        
+            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+            top = max(top, labelSize[1])
+            cv2.rectangle(frame, (left, top - labelSize[1]), (left + labelSize[0], top + baseLine), (255, 255, 255), cv2.FILLED)
+            cv2.putText(frame, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+            cv2.imshow("frame",frame)
+            cv2.waitKey(0)
+        '''
 
         classIds = []
         confidences = []
@@ -52,6 +56,7 @@ def Object_detect(frame,confThreshold=0.4,nmsThreshold=0.3):
         global df
         df=pd.DataFrame(columns=["ClassIds","Confidences","TLpoint","BRpoint"]) 
         
+        #Appending locations of each detected object onto a dataframe df
         for i in indices:
             box = boxes[i]
             left = box[0]
@@ -61,8 +66,10 @@ def Object_detect(frame,confThreshold=0.4,nmsThreshold=0.3):
             df=df.append({"ClassIds":classIds[i],"Confidences":confidences[i],"TLpoint":[left, top],"BRpoint":[left + width, top + height]},ignore_index=True)
             # drawPred(classIds[i], confidences[i], left, top, left + width, top + height)
 
+    #create blob from input image
     blob=cv2.dnn.blobFromImage(frame,1/255,(1280,1280),(0,0,0),swapRB=True,crop=False)
 
+    #inputing blob into neural network
     net.setInput(blob)
     outs=net.forward(outNames)
     postprocess(frame,outs)
